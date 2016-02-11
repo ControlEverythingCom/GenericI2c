c4d5958cec4f3e25b9dd1b7abeb963bc549968ec
zaphod-42 <trey@tomadoh.com>
2016-02-11 12:38:20 -0600

    Update i2c-control.ino

--- firmware/examples/i2c-control.ino
+++ firmware/examples/i2c-control.ino
@@ -1,63 +1,67 @@
-#include "i2c-devices.h"
+// This #include statement was automatically added by the Particle IDE.
+#include "GenericI2c/GenericI2c.h"
 
-String myLog = "";
+//Set variables for Particle cloud
 String temperature = "";
 String relayStatus = "";
-i2cDevices devices;
-int counter = 0;
 
-int scanDevices(String a);
-int sendI2cCommand(String a);
-int readI2cCommand(String a);
-int setTemp(String a);
+//Instantiate I2C class
+GenericI2c devices;
+
+//Set addresses for connected devices
+int relayAddr = 32;
+int tempAddr = 72;
+
 void processTemp(int vals[]);
+void processRelays(int val[])
 
 void setup() {
-    Particle.function("findDevices", scanDevices);
-    Particle.function("sendCommand", sendI2cCommand);
-    Particle.function("readCommand", readI2cCommand);
     Particle.variable("relayStatus", relayStatus);
     Particle.variable("Temperature", temperature);
-    Particle.variable("myLog", myLog);
+
+    //Initialize I2C communication
     devices.init();
+
+    //Add devices for use
+    devices.addDevice(relayAddr, "0,252|6,252");
+    devices.addDevice(tempAddr, "1,96");
+
+    //initialize devices
     devices.initDevices();
 }
 
 void loop() {
     if(millis()%1000==0){
-        devices.readI2cCommand("72,0,2", processTemp);
-        devices.readI2cCommand("32,10,1", processRelays);
-        myLog=devices.updateLog();
+        //Once a second check the temperature and relay status
+        devices.readI2cCommand(tempAddr, "0,2", processTemp);
+        devices.readI2cCommand(relayAddr, "10,1", processRelays);
     }
 }
 
-int scanDevices(String a){
-    devices.scan();
-    return 1;
-}
-
-int sendI2cCommand(String a){
-    return devices.sendCommands(a);
-}
-
-int readI2cCommand(String a){
-    return devices.readI2cCommand(a);
-}
-
+//Callback function for reading temperature
 void processTemp(int vals[]){
-    
+    //Process temperature to get a real world value
     float temp = vals[0];
     temp += (float)vals[1]/256.00;
-    
     float tempF = temp*1.8+32;
-    
+
+    //Set the temperature variable
     temperature = String(tempF,2);
+
+    if(tempF > 73){
+        //If the temperature in Fahrenheit is greater than 73, turn on both relays
+        devices.sendCommand(relayAddr, "10,3");
+    }else{
+        //Otherwise turn them both off
+        devices.sendCommand(relayAddr, "10,0");
+    }
 }
 
+//Callback function for checking relay status
 void processRelays(int val[]){
     relayStatus = val[0];
     relayStatus += " - Relay 1 is ";
-    
+
     if(val[0] == 3 || val[0] == 1) {
         relayStatus += "on";
     }else{
