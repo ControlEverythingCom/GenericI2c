fbbe6d5a1424e2a7b0e152dd803c892aa3fdb4f4
zaphod-42 <trey@tomadoh.com>
2016-02-11 12:52:22 -0600

    Update GenericI2c.h

--- firmware/GenericI2c.h
+++ firmware/GenericI2c.h
@@ -7,6 +7,7 @@ int indexOfNth(String str, char c, int ind);
 
 class i2cDevices{
 public:
+    void i2cDevices();
     void init();
     bool addDevice(int address);
     bool addDevice(int address, String initCmds);
